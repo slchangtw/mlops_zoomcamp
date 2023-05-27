46f32ec270f30babe307698461a6e791bfe46ee7
Shun-Lung Chang <shun_lung_chang@SLs-Macbook.local>
2023-05-27 21:13:26 +0200

    Refector 01-intro

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .load_data import read_trips
+from .preprocess import process_trips
+
+__all__ = ["read_trips", "process_trips"]
---
+++ src/load_data.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import pandas as pd
+import wget
+
+TLC_TRIP_DATA_URL = "https://d37ci6vzurychx.cloudfront.net/trip-data/"
+
+
+def read_trips(data_folder: Path, color: str, year: str, month: str) -> pd.DataFrame:
+    if not data_folder.exists():
+        data_folder.mkdir(parents=True, exist_ok=True)
+
+    data_path = data_folder / f"{color}_tripdata_{year}-{month:>02}.parquet"
+    if not data_path.exists():
+        url = f"{TLC_TRIP_DATA_URL}{color}_tripdata_{year}-{month}.parquet"
+        wget.download(url, str(data_path))
+
+    return pd.read_parquet(data_path)
