6a32d7926e8c2947330bc65b538c205d9c5c403e
Shun-Lung Chang <shun_lung_chang@SLs-Macbook.local>
2023-06-17 21:14:34 +0200

    Add serve

--- 04-deployment/test_app.py
+++ 04-deployment/test_app.py
@@ -1,11 +1,18 @@
 import requests
+import typer
 
-trips_params = {
-    "color": "yellow",
-    "year": "2022",
-    "month": "02",
-}
+app = typer.Typer()
 
-url = "http://localhost:9696/predict"
-response = requests.post(url, json=trips_params)
-print(response.json())
+@app.command()
+def send_request(color: str, year: str, month: str) -> None:
+    trips_params = {
+        "color": color,
+        "year": year,
+        "month": month,
+    }
+    url = "http://localhost:9696/predict"
+    response = requests.post(url, json=trips_params)
+    print(response.json())
+
+if __name__ == "__main__":
+    app()
